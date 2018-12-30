f74b8985fb8e37358b70e1a38d6c3e13818d0ed9
Alexandru Cambose <alexcambose@yahoo.com>
2018-12-30 23:51:51 +0200

    Library with tests

---
+++ index.js
@@ -0,0 +1,92 @@
+class CssFunc {
+  constructor($element, attr) {
+    this.$element = $element;
+    this.attr = attr;
+    this._functionObject = this._getFunctions(this._getValue($element, attr));
+  }
+  _getValue($element, attr) {
+    return window.getComputedStyle($element).getPropertyValue(attr);
+  }
+  _setValue(value) {
+    this.$element.style[this.attr] = value;
+  }
+  /**
+   * Generates a string from object functions
+   * @param  {object} object
+   */
+  _getValueFromObject(object) {
+    let valueString = '';
+    Object.keys(object).forEach(functionName => {
+      valueString += `${functionName}(${object[functionName].join(', ')}) `;
+    });
+    return valueString.substring(0, valueString.length - 1);
+  }
+
+  /**
+   * Returns an object containing functions from the string
+   * @param  {object} functionsString
+   */
+  _getFunctions(functionsString) {
+    const regex = /(\w+)\((.*?)\)/; // .*? for ungreedy
+    const values = functionsString.split(regex);
+    let functions = {};
+    for (let i = 1; i < values.length; i += 3) {
+      functions[values[i]] = values[i + 1].replace(' ', '').split(',');
+    }
+    return functions;
+  }
+  /**
+   * Get value from element's property
+   */
+  get() {
+    return this._getValue(this.$element, this.attr);
+  }
+  /**
+   * @param  {string} property
+   * @param  {string|array} value
+   * @param  {boolean} autoUpdate=true
+   */
+  add(property, value, autoUpdate = true) {
+    if (!this.exists(property)) {
+      if (typeof value === 'string') value = [value];
+      this._functionObject[property] = value;
+      this._setValue(this._getValueFromObject(this._functionObject));
+      return true;
+    } else if (autoUpdate) {
+      // if autoUpdate automatically update the property if it doesn't exist
+      this.update(property, value);
+      return true;
+    }
+    return false;
+  }
+  /**
+   * @param  {string} property
+   * @param  {string|array} value
+   * @param  {boolean} autoAdd=true
+   */
+  update(property, value, autoAdd = true) {
+    if (this.exists(property)) {
+      // if the provided value is a string convert it in an array
+      if (typeof value === 'string') value = [value];
+      // add the property to the functions object
+      this._functionObject[property] = value;
+      this._setValue(this._getValueFromObject(this._functionObject));
+      return true;
+    } else if (autoAdd) {
+      // if autoAdd automatically add the property if it doesn't exist
+      this.add(property, value);
+      return true;
+    }
+    return false;
+  }
+  /**
+   * Returns true if property exists
+   * @param  {string} property
+   */
+  exists(property) {
+    // check if object has property
+    return this._functionObject.hasOwnProperty(property);
+  }
+}
+
+module.exports = ($element, attr) => new CssFunc($element, attr);
--- index.test.js
+++ index.test.js
@@ -1,1 +1,108 @@
-module.exports = () => {};
+const Cssfunc = require('./index');
+document.body.innerHTML = `<p>test</p>`;
+const $element = document.querySelector('p');
+const transformValue = 'translate(10px, 20px) scale(1.1)rotate(45deg)';
+$element.style.transform = transformValue;
+
+describe('css-func', () => {
+  let cssFunc = Cssfunc($element, 'transform');
+
+  describe('_getValue()', () => {
+    it('gets the value of the style property', () => {
+      expect(cssFunc._getValue($element, 'transform')).toEqual(
+        $element.style.transform
+      );
+    });
+  });
+  describe('_setValue()', () => {
+    it('sets the value to the dom element', () => {
+      cssFunc._setValue('translate(10px, 20px)');
+      expect($element.style.transform).toEqual('translate(10px, 20px)');
+      $element.style.transform = transformValue;
+    });
+  });
+  describe('_getFunctions()', () => {
+    expect(cssFunc._getFunctions(transformValue)).toEqual({
+      translate: ['10px', '20px'],
+      scale: ['1.1'],
+      rotate: ['45deg'],
+    });
+  });
+  describe('get()', () => {
+    it('gets the value of the style property', () => {
+      expect(cssFunc.get()).toEqual($element.style.transform);
+    });
+  });
+  describe('exists()', () => {
+    it('returns true if the element has a specified function name', () => {
+      expect(cssFunc.exists('translate')).toBeTruthy();
+      expect(cssFunc.exists('rotate')).toBeTruthy();
+      expect(cssFunc.exists('scale')).toBeTruthy();
+    });
+    it("returns false if the element doesn't have a specified function name", () => {
+      expect(cssFunc.exists('matrix')).toBeFalsy();
+    });
+  });
+  describe('_getValueFromObject()', () => {
+    it('returns a string contaiuning the functions', () => {
+      expect(
+        cssFunc._getValueFromObject({
+          translate: ['10px', '20px'],
+          scale: ['1.1'],
+          rotate: ['45deg'],
+        })
+      ).toEqual('translate(10px, 20px) scale(1.1) rotate(45deg)');
+    });
+  });
+  describe('update()', () => {
+    afterEach(() => {
+      $element.style.transform = transformValue;
+      cssFunc = Cssfunc($element, 'transform');
+    });
+    const object = { ...cssFunc._functionObject };
+    it('updates internal functions object', () => {
+      expect(cssFunc.update('rotate', ['90deg'])).toBeTruthy();
+      expect(cssFunc._functionObject).toEqual({ ...object, rotate: ['90deg'] });
+    });
+    it("automatically adds the property if it doesn't exist", () => {
+      expect(cssFunc.update('rotateY', '90deg')).toBeTruthy();
+      expect(cssFunc._functionObject).toEqual({
+        ...object,
+        rotateY: ['90deg'],
+      });
+    });
+    it('updates the dom element style property', () => {
+      expect(cssFunc.update('rotateY', '90deg')).toBeTruthy();
+      expect($element.style.transform).toEqual(
+        'translate(10px, 20px) scale(1.1) rotate(45deg) rotateY(90deg)'
+      );
+    });
+  });
+  describe('add()', () => {
+    afterEach(() => {
+      $element.style.transform = transformValue;
+      cssFunc = Cssfunc($element, 'transform');
+    });
+    const object = { ...cssFunc._functionObject };
+    it('adds the property to the functions object', () => {
+      expect(cssFunc.add('rotateY', '90deg')).toBeTruthy();
+      expect(cssFunc._functionObject).toEqual({
+        ...object,
+        rotateY: ['90deg'],
+      });
+    });
+    it('automatically updates the property if it is aleardy present', () => {
+      expect(cssFunc.add('rotate', '90deg')).toBeTruthy();
+      expect(cssFunc._functionObject).toEqual({
+        ...object,
+        rotate: ['90deg'],
+      });
+    });
+    it('updates the dom element style property', () => {
+      expect(cssFunc.update('rotateY', '90deg')).toBeTruthy();
+      expect($element.style.transform).toEqual(
+        'translate(10px, 20px) scale(1.1) rotate(45deg) rotateY(90deg)'
+      );
+    });
+  });
+});
